81b51255de641d544b9fa375584a1a1fabe5ba44
Ya.nnis <ybarache@icloud.com>
2025-01-16 11:39:37 +0100

    ✨ #3 : Question 3 et 4 OK

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use image::{io::Reader as ImageReader, DynamicImage, ImageOutputFormat, RgbImage};
+use std::fs::File;
+use std::io::{Cursor, Write};
+
+fn open_image() -> DynamicImage {
+    ImageReader::open("./img/PNG_canal_alpha.png")
+        .expect("Impossible d'ouvrir l'image")
+        .decode()
+        .expect("Impossible de décoder l'image")
+}
+
+fn save_image_rgb8(img: DynamicImage) {
+    let img = img.to_rgb8();
+    img.save("./img/RGB8_canal_alpha.png").expect("Impossible de sauvegarder l'image");
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let img = open_image();
+    println!("Image ouverte avec succès, dimensions : {}x{}", img.width(), img.height());
+
+    save_image_rgb8(img);
+    println!("Image sauvegardée avec succès");
 }
